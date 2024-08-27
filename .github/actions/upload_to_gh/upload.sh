6a87bdece0657bb9e0a2ca0f127b4e930bf015bd
Zheng-Bote <zheng.bote@googlemail.com>
2024-08-27 15:56:26 +0200

    dev: upload to GH

--- .github/actions/upload_to_gh/upload.sh
+++ .github/actions/upload_to_gh/upload.sh
@@ -5,6 +5,8 @@ echo "para 2: $2"
 echo "para 3: $3"
 
 git switch $1
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git pull
 cp -R $2 $3/.
 git add $3/*
